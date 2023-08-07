03867b76f9b7589c19823b449f0bf5733cc7b5b2
ItzzExcel <excel.xcel@icloud.com>
2023-08-07 01:25:11 -0600

    Forgot this.

--- FluxAPI/Properties/AssemblyInfo.cs
+++ FluxAPI/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("09.07.2023")]
-[assembly: AssemblyFileVersion("09.07.2023")]
+[assembly: AssemblyVersion("07.08.2023")]
+[assembly: AssemblyFileVersion("07.08.2023")]
